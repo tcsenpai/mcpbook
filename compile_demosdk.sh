dbd4fdbcc4deb8bb5ebd8a507c04122aec1dc8d2
tcsenpai <tcsenpai@discus.sh>
2025-07-07 12:13:32 +0200

    added instructions

--- compile_demosdk.sh
+++ compile_demosdk.sh
@@ -2,7 +2,12 @@
 cp .env.demosdk .env
 npm install
 npm run build
+chmod +x dist/index.js
+
 
 echo "Demosdk compiled successfully."
 echo "To run the Demos SDK MCP server, use the command:"
 echo "npm run start"
+echo "You can also add the mcp server to any tool by pointing them to the dist/index.js file."
+echo "For example, in claude code you can use:"
+echo "claude mcp add /path/to/demosdk_mcp_server/dist/index.js"
